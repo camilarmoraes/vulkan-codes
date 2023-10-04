c9a080f09da079ac13cd3b5c2f19f5c905db0c8d
camilarmoraes <camilaribeiro0308@gmail.com>
2023-10-04 11:14:54 -0300

    primeiro triangulo

---
+++ first_app.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "lve_window.hpp"
+#include "lve_pipeline.hpp"
+#include "lve_device.hpp"
+#include "lve_swap_chain.hpp"
+
+
+#include <memory>
+#include <vector>
+
+namespace lve{
+    
+    class FirstApp{
+
+        public:
+            static constexpr int WIDTH = 800;
+            static constexpr int HEIGHT = 600;
+
+            FirstApp();
+            ~FirstApp();
+
+            FirstApp(const LveWindow &) = delete;
+            FirstApp &operator=(const LveWindow &) = delete;
+
+            void run();
+        private:
+            void createPipelineLayout();
+            void createPipeline();
+            void createCommandBuffers();
+            void drawFrame();
+
+            LveWindow lveWindow{WIDTH, HEIGHT,  "hello"};
+            LveDevice lveDevice{lveWindow};
+            LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtent()};
+            std::unique_ptr<LvePipeline> lvePipeline; //vector <memory>
+            VkPipelineLayout pipelineLayout;
+            std::vector<VkCommandBuffer> commandBuffers;
+
+    };
+}
