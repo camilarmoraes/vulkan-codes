79d2abbc8ffc6c61c4a69c582fc0f095ff71689c
camilarmoraes <camilaribeiro0308@gmail.com>
2023-10-23 21:35:50 -0300

    rotações em um cubo

--- simple_render_system.cpp
+++ simple_render_system.cpp
@@ -66,7 +66,7 @@ void SimpleRenderSystem::renderGameObjects(
   lvePipeline->bind(commandBuffer);
 
   for (auto& obj : gameObjects) {
-    obj.transform2d.rotation = glm::mod(obj.transform2d.rotation + 0.01f, glm::two_pi<float>());
+    obj.transform2d.rotation = glm::mod(obj.transform2d.rotation + 0.0001f, glm::two_pi<float>());
 
     SimplePushConstantData push{};
     push.offset = obj.transform2d.translation;
