0e108f8fde9ba9999d4169b5c312d75f12e6552d
camilarmoraes <camilaribeiro0308@gmail.com>
2023-10-23 22:04:38 -0300

    matrizes de projeção + view camera

--- simple_render_system.cpp
+++ simple_render_system.cpp
@@ -60,8 +60,7 @@ void SimpleRenderSystem::createPipeline(VkRenderPass renderPass) {
       pipelineConfig);
 }
 
-void SimpleRenderSystem::renderGameObjects(
-    VkCommandBuffer commandBuffer, std::vector<LveGameObject>& gameObjects) {
+void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::vector<LveGameObject>&gameObjects, const LveCamera& camera) {
   lvePipeline->bind(commandBuffer);
 
   for (auto& obj : gameObjects) {
@@ -70,7 +69,7 @@ void SimpleRenderSystem::renderGameObjects(
 
     SimplePushConstantData push{};
     push.color = obj.color;
-    push.transform = obj.transform.mat4();
+    push.transform = camera.getProjection() * obj.transform.mat4();
 
     vkCmdPushConstants(
         commandBuffer,
